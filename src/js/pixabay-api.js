619ac16274ca44172c11b79707d563766236a3bd
Nastya <nastya.syasyafi@gmail.com>
2024-04-21 18:52:24 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '43211197-e02e136a3ee46bcda7d5bf66f';
+const API_KEY = '43500562-b59296b97b4360198cb07a703';
 
 export async function searchObject(searchName, page) {
   const { data } = await axios(`${BASE_URL}`, {
